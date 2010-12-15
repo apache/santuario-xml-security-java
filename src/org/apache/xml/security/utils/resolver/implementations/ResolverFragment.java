ce2ea3c9af2f7476a2d5bcf41a6820bf1a892394
Colm O hEigeartaigh <coheigea@apache.org>
2010-12-15 11:15:55 +0000

    Moving branches to xml-security-java/branches

---
+++ src/org/apache/xml/security/utils/resolver/implementations/ResolverFragment.java
@@ -0,0 +1,279 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 1999 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "<WebSig>" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation and was
+ * originally based on software copyright (c) 2001, Institute for
+ * Data Communications Systems, <http://www.nue.et-inf.uni-siegen.de/>.
+ * The development of this software was partly funded by the European
+ * Commission in the <WebSig> project in the ISIS Programme.
+ * For more information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+package org.apache.xml.security.utils.resolver.implementations;
+
+
+
+import java.util.Set;
+
+import org.apache.xml.security.c14n.Canonicalizer;
+import org.apache.xml.security.signature.XMLSignatureInput;
+import org.apache.xml.security.utils.HelperNodeList;
+import org.apache.xml.security.utils.IdResolver;
+import org.apache.xml.security.utils.XMLUtils;
+import org.apache.xml.security.utils.resolver.ResourceResolverException;
+import org.apache.xml.security.utils.resolver.ResourceResolverSpi;
+import org.apache.xml.utils.URI;
+import org.apache.xpath.CachedXPathAPI;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+
+/**
+ * This resolver is used for resolving same-document URIs like URI="" of URI="#id".
+ *
+ * @author $Author$
+ * @see <A HREF="http://www.w3.org/TR/xmldsig-core/#sec-ReferenceProcessingModel">The Reference processing model in the XML Signature spec</A>
+ * @see <A HREF="http://www.w3.org/TR/xmldsig-core/#sec-Same-Document">Same-Document URI-References in the XML Signature spec</A>
+ * @see <A HREF="http://www.ietf.org/rfc/rfc2396.txt">Section 4.2 of RFC 2396</A>
+ */
+public class ResolverFragment extends ResourceResolverSpi {
+
+   /** {@link org.apache.commons.logging} logging facility */
+    static org.apache.commons.logging.Log log = 
+        org.apache.commons.logging.LogFactory.getLog(
+                            ResolverFragment.class.getName());
+
+   /**
+    * Method engineResolve
+    *
+    * Wird das gleiche Dokument referenziert?
+    * Wird ein anderes Dokument referenziert?
+    *
+    * @param uri
+    * @param BaseURI
+    *
+    * @throws ResourceResolverException
+    */
+   public XMLSignatureInput engineResolve(Attr uri, String BaseURI)
+           throws ResourceResolverException {
+
+      String uriNodeValue = uri.getNodeValue();
+      NodeList resultNodes = null;
+      Document doc = uri.getOwnerDocument();
+
+      // this must be done so that Xalan can catch ALL namespaces
+      XMLUtils.circumventBug2650(doc);
+
+      CachedXPathAPI cXPathAPI = new CachedXPathAPI();
+
+      if (uriNodeValue.equals("")) {
+
+         /*
+          * Identifies the node-set (minus any comment nodes) of the XML
+          * resource containing the signature
+          */
+
+         log.debug("ResolverFragment with empty URI (means complete document)");
+         try {
+            resultNodes =
+               cXPathAPI.selectNodeList(doc,
+                                        Canonicalizer.XPATH_C14N_OMIT_COMMENTS);
+         } catch (javax.xml.transform.TransformerException ex) {
+            throw new ResourceResolverException("generic.EmptyMessage", ex,
+                                                uri, BaseURI);
+         }
+      } else {
+
+         /*
+          * URI="#chapter1"
+          * Identifies a node-set containing the element with ID attribute
+          * value 'chapter1' of the XML resource containing the signature.
+          * XML Signature (and its applications) modify this node-set to
+          * include the element plus all descendents including namespaces and
+          * attributes -- but not comments.
+          */
+         String id = uriNodeValue.substring(1);
+
+         // Element selectedElem = doc.getElementById(id);
+         Element selectedElem = IdResolver.getElementById(doc, id);
+
+         log.debug("Try to catch an Element with ID " + id + " and Element was " + selectedElem);
+         if (selectedElem == null) {
+            resultNodes = new HelperNodeList();
+         } else {
+            try {
+               resultNodes =
+                  cXPathAPI
+                     .selectNodeList(selectedElem, Canonicalizer
+                        .XPATH_C14N_OMIT_COMMENTS_SINGLE_NODE);
+            } catch (javax.xml.transform.TransformerException ex) {
+               throw new ResourceResolverException("generic.EmptyMessage", ex,
+                                                   uri, BaseURI);
+            }
+         }
+      }
+
+      Set resultSet = XMLUtils.convertNodelistToSet(resultNodes);
+      XMLSignatureInput result = new XMLSignatureInput(resultSet, cXPathAPI);
+
+      log.debug("We return a nodeset with " + resultNodes.getLength() + " nodes");
+      result.setMIMEType("text/xml");
+
+      try {
+         URI uriNew = new URI(new URI(BaseURI), uri.getNodeValue());
+
+         result.setSourceURI(uriNew.toString());
+      } catch (URI.MalformedURIException ex) {
+         result.setSourceURI(BaseURI);
+      }
+
+      return result;
+   }
+
+   /**
+    * Method engineCanResolve
+    *
+    * @param uri
+    * @param BaseURI
+    *
+    */
+   public boolean engineCanResolve(Attr uri, String BaseURI) {
+
+      if (uri == null) {
+         log.debug("Quick fail for null uri");
+         return false;
+      }
+
+      String uriNodeValue = uri.getNodeValue();
+
+      if (uriNodeValue.equals("")
+              || (uriNodeValue.startsWith("#")
+                  &&!uriNodeValue.startsWith("#xpointer("))) {
+         log.debug("State I can resolve reference: \"" + uriNodeValue + "\"");
+         return true;
+      }
+      log.debug("Do not seem to be able to resolve reference: \"" + uriNodeValue + "\"");
+      return false;
+   }
+
+   /**
+    * Method isSameDocumentReference
+    *
+    * @param uri
+    * @param BaseURI
+    *
+    * @throws URI.MalformedURIException
+    */
+   private static boolean isSameDocumentReference(Attr uri, String BaseURI)
+           throws URI.MalformedURIException {
+
+      String uriNodeValue = uri.getNodeValue();
+
+      if (uriNodeValue.equals("") || uriNodeValue.startsWith("#")) {
+         return true;
+      } else {
+         return false;
+      }
+   }
+
+   /**
+    * Method selectIdentifiedPortions
+    *
+    * @param uri
+    * @param BaseURI
+    * @param input
+    *
+    * @throws ResourceResolverException
+    * private static XMLSignatureInput selectIdentifiedPortions(
+    *       Attr uri, String BaseURI, XMLSignatureInput input)
+    *          throws ResourceResolverException {
+    *
+    *  try {
+    *     log.debug("Got " + input.getBytes().length
+    *               + " bytes from source and  URI oif " + input.getSourceURI());
+    *  } catch (Exception ex) {
+    *     throw new ResourceResolverException("generic.EmptyMessage", ex, uri,
+    *                                         BaseURI);
+    *  }
+    *
+    *  try {
+    *     URI fragmentURI = new URI(new URI(BaseURI), uri.getNodeValue());
+    *     String fragment = fragmentURI.getFragment();
+    *     javax.xml.parsers.DocumentBuilderFactory dbf =
+    *        javax.xml.parsers.DocumentBuilderFactory.newInstance();
+    *
+    *     dbf.setNamespaceAware(true);
+    *     dbf.setValidating(true);
+    *
+    *     javax.xml.parsers.DocumentBuilder db = dbf.newDocumentBuilder();
+    *     Document doc =
+    *        db.parse(new java.io.ByteArrayInputStream(input.getBytes()));
+    *
+    *     log.debug("Owner Document " + doc.getDocumentElement().getTagName());
+    *     log.debug("try to select fragment " + fragment);
+    *
+    *     // Element selectedElem = doc.getElementById(fragment);
+    *     Element selectedElem = IdResolver.getElementById(doc, fragment);
+    *
+    *     if (selectedElem != null) {
+    *        log.debug("Selection hat geklappt!!!: "
+    *                  + selectedElem.getTagName());
+    *     }
+    *
+    *     return input;
+    *  } catch (Exception ex) {
+    *     throw new ResourceResolverException("generic.EmptyMessage", ex, uri,
+    *                                         BaseURI);
+    *  }
+    * }
+    */
+}
---
+++ src_unitTests/org/apache/xml/security/test/encryption/BaltimoreEncTest.java
@@ -0,0 +1,363 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 1999 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "<WebSig>" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation and was
+ * originally based on software copyright (c) 2001, Institute for
+ * Data Communications Systems, <http://www.nue.et-inf.uni-siegen.de/>.
+ * The development of this software was partly funded by the European
+ * Commission in the <WebSig> project in the ISIS Programme.
+ * For more information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+package org.apache.xml.security.test.encryption;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESedeKeySpec;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.apache.xml.security.encryption.EncryptedData;
+import org.apache.xml.security.encryption.XMLCipher;
+import org.apache.xml.security.keys.KeyInfo;
+import org.apache.xml.security.keys.content.KeyName;
+import org.apache.xml.security.utils.XMLUtils;
+import org.apache.xml.serialize.DOMSerializer;
+import org.apache.xml.serialize.Method;
+import org.apache.xml.serialize.OutputFormat;
+import org.apache.xml.serialize.XMLSerializer;
+import org.apache.xpath.XPathAPI;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+/**
+ * Interop test for XML Encryption
+ *
+ * @author Berin Lautenbach
+ */
+public class BaltimoreEncTest extends TestCase {
+
+	private static String cardNumber;
+	private static int nodeCount = 0;
+	private static final byte[] bobBytes = 
+		"abcdefghijklmnopqrstuvwx".getBytes();
+
+
+	/** {@link org.apache.commons.logging} logging facility */
+    static org.apache.commons.logging.Log log = 
+        org.apache.commons.logging.LogFactory.getLog(BaltimoreEncTest.class.getName());
+	
+	/**
+	 * Method suite
+	 *
+	 *
+	 */
+	public static Test suite() throws Exception {
+		return new TestSuite(BaltimoreEncTest.class);
+	}
+
+	/**
+	 *  Constructor BaltimoreEncTest
+	 *
+	 *  @param Name_
+	 */
+	public BaltimoreEncTest(String Name_) {
+		super(Name_);
+	}
+	
+	/**
+	 * Method main
+	 *
+	 * @param args
+	 */
+
+	protected void setUp() throws Exception {
+		
+		String[] testCaseName = { "-noloading",
+								  BaltimoreEncTest.class.getName() };
+
+		// Create the comparison strings
+
+		DocumentBuilderFactory dbf =
+			DocumentBuilderFactory.newInstance();
+		dbf.setNamespaceAware(true);
+		dbf.setAttribute("http://xml.org/sax/features/namespaces", Boolean.TRUE);
+		
+		String filename = 
+			"data/ie/baltimore/merlin-examples/merlin-xmlenc-five/plaintext.xml";
+		File f = new File(filename);
+
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		Document doc = db.parse(new java.io.FileInputStream(f));
+
+		cardNumber = retrieveCCNumber(doc);
+
+		// Count the nodes in the document as a secondary test
+		nodeCount = countNodes(doc);
+
+		// Initialise the library and get out of here
+
+		org.apache.xml.security.Init.init();
+	}
+
+	/**
+	 * Method retrieveCCNumber
+	 *
+	 * Retrieve the credit card number from the payment info document
+	 *
+	 * @param doc The document to retreive the card number from
+	 * @return The retrieved credit card number
+	 */
+
+	public static String retrieveCCNumber (Document doc) 
+		throws javax.xml.transform.TransformerException {
+
+		Element nscontext =
+			XMLUtils.createDSctx(doc, "x",
+                              "urn:example:po");
+		Node ccnumElt = 
+			XPathAPI.selectSingleNode(doc, "//x:Number/text()", nscontext);
+		
+		if (ccnumElt != null)
+			return ccnumElt.getNodeValue();
+
+		return null;
+
+	}
+
+	/**
+	 * Method test_five_content_3des_cbc
+	 *
+	 * Check the merlin-enc-five element content test for 3DES
+	 *
+	 */
+
+	public void test_five_content_3des_cbc() throws Exception {
+
+		String filename = "data/ie/baltimore/merlin-examples/merlin-xmlenc-five/encrypt-content-tripledes-cbc.xml";
+
+		Document dd = decryptElement(filename, XMLCipher.TRIPLEDES);
+
+		String cc = retrieveCCNumber(dd);
+
+		// Compare the retrieved number to the stored number
+
+		assertTrue(cc, ((cc != null) && (cc.equals(cardNumber))));
+		
+		// Test my numbers
+
+		int myNodeCount = countNodes(dd);
+
+		assertTrue("Node count mismatches", 
+				   ((myNodeCount > 0) && myNodeCount == nodeCount));
+    }
+
+	/**
+	 * Method decryptElement
+	 *
+	 * Take a key, encryption type and a file, find an encrypted element
+	 * decrypt it and return the resulting document
+	 *
+	 * @param filename File to decrypt from
+	 * @param encType String representing the encryption type
+	 * @param key Key to use for decryption
+	 */
+
+	public Document decryptElement (String filename, String encType) 
+		throws Exception {
+
+		XMLCipher cipher;
+		
+		// Parse the document in question
+
+		javax.xml.parsers.DocumentBuilderFactory dbf =
+			javax.xml.parsers.DocumentBuilderFactory.newInstance();
+		dbf.setNamespaceAware(true);
+		dbf.setAttribute("http://xml.org/sax/features/namespaces", Boolean.TRUE);
+		File f = new File(filename);
+
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		
+		Document doc = db.parse(new java.io.FileInputStream(f));
+		
+		// Now we have the document, lets build the XMLCipher element
+
+		Element ee = null;
+		
+		// Create the XMLCipher element
+		
+		cipher = XMLCipher.getInstance(encType);
+
+		// Need to pre-load the Encrypted Data so we can get the key info
+
+		ee = (Element) doc.getElementsByTagName("EncryptedData").item(0);
+		EncryptedData encryptedData = cipher.loadEncryptedData(doc, ee);
+		KeyInfo ki = encryptedData.getKeyInfo();
+   
+		SecretKey key = null;
+
+		if (ki != null) {
+			KeyName keyName = ki.itemKeyName(0);
+			if (keyName != null) {
+				key = mapKeyName(keyName.getKeyName());
+			}
+		}
+		cipher.init(XMLCipher.DECRYPT_MODE, key);
+		Document dd = cipher.doFinal(doc, ee);
+
+		return dd;
+			
+    }
+
+	/** 
+	 * Method mapKeyName
+	 *
+	 * Create a secret key from a key name for merlin-five
+	 *
+	 * @param name Name to map a key from
+	 */
+
+	public SecretKey mapKeyName(String name) throws Exception {
+
+		if (name.equals("bob")) {
+
+			// Bob is a DESEDE key
+
+			DESedeKeySpec keySpec = new DESedeKeySpec(bobBytes);
+			SecretKeyFactory keyFactory = 
+				SecretKeyFactory.getInstance("DESede");
+			SecretKey key = keyFactory.generateSecret(keySpec);
+
+			return key;
+
+		}
+
+		return null;
+
+	}
+
+	/**
+	 * Method countNodes
+	 *
+	 * Recursively count the number of nodes in the document
+	 *
+	 * @param n Node to count beneath
+	 */
+
+	private static int countNodes(Node n) {
+
+		if (n == null)
+			return 0;  // Paranoia
+
+		int count = 1;  // Always count myself
+		Node c = n.getFirstChild();
+
+		while (c != null) {
+
+			count += countNodes(c);
+			c = c.getNextSibling();
+
+		}
+
+		return count;
+
+	}
+
+    private static void dump(Document document) {
+        OutputFormat of = new OutputFormat();
+        of.setIndenting(true);
+        of.setMethod(Method.XML);
+        of.setOmitDocumentType(true);
+        of.setOmitXMLDeclaration(true);
+        DOMSerializer serializer = new XMLSerializer(System.out, of);
+        try {
+            serializer.serialize(document);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    private static String toString(Element element) {
+        OutputFormat of = new OutputFormat();
+        of.setIndenting(true);
+        of.setMethod(Method.XML);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DOMSerializer serializer = new XMLSerializer(baos, of);
+        try {
+            serializer.serialize(element);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return (baos.toString());
+    }
+
+    private static String toString(Document document) {
+        OutputFormat of = new OutputFormat();
+        of.setIndenting(true);
+        of.setMethod(Method.XML);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DOMSerializer serializer = new XMLSerializer(baos, of);
+        try {
+            serializer.serialize(document);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return (baos.toString());
+    }
+}
