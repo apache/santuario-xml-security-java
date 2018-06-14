f5145dc7a6e4deaf63a689558eda25e742cb2ae0
Sean Mullan <mullan@apache.org>
2018-06-14 16:44:51 +0000

    The supportedKeyTypes field should be private.

--- src/main/java/org/apache/xml/security/keys/content/DEREncodedKeyValue.java
+++ src/main/java/org/apache/xml/security/keys/content/DEREncodedKeyValue.java
@@ -37,7 +37,7 @@ import org.w3c.dom.Element;
 public class DEREncodedKeyValue extends Signature11ElementProxy implements KeyInfoContent {
 
     /** JCA algorithm key types supported by this implementation. */
-    static final String supportedKeyTypes[] = { "RSA", "DSA", "EC"};
+    private static final String supportedKeyTypes[] = { "RSA", "DSA", "EC"};
 
     /**
      * Constructor DEREncodedKeyValue
