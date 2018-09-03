86fa249de05e03b7d8f9d7a4ee40ba0adfd685e2
Colm O hEigeartaigh <coheigea@apache.org>
2018-09-03 12:01:21 +0000

    Changing the ForbiddenRefCountTest to work dynamically rather than reading in a signature file

--- src/test/java/org/apache/xml/security/test/dom/secure_val/ForbiddenRefCountTest.java
+++ src/test/java/org/apache/xml/security/test/dom/secure_val/ForbiddenRefCountTest.java
@@ -18,16 +18,26 @@
  */
 package org.apache.xml.security.test.dom.secure_val;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Enumeration;
+
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.xml.security.exceptions.XMLSecurityException;
-import org.apache.xml.security.signature.Manifest;
+import org.apache.xml.security.signature.XMLSignature;
+import org.apache.xml.security.signature.XMLSignatureException;
 import org.apache.xml.security.test.dom.interop.InteropTestBase;
+import org.apache.xml.security.transforms.Transforms;
 import org.apache.xml.security.utils.Constants;
 import org.apache.xml.security.utils.XMLUtils;
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 
@@ -35,6 +45,12 @@ import org.w3c.dom.Element;
  * This is a test for a forbidden number of references when secure validation is enabled.
  */
 public class ForbiddenRefCountTest extends InteropTestBase {
+
+    private static final String BASEDIR =
+        System.getProperty("basedir") == null ? "./": System.getProperty("basedir");
+    public static final String KEYSTORE_DIRECTORY = BASEDIR + "/src/test/resources/";
+    public static final String KEYSTORE_PASSWORD_STRING = "changeit";
+    public static final char[] KEYSTORE_PASSWORD = KEYSTORE_PASSWORD_STRING.toCharArray();
 
     static org.slf4j.Logger LOG =
         org.slf4j.LoggerFactory.getLogger(ForbiddenRefCountTest.class);
@@ -49,36 +65,82 @@ public class ForbiddenRefCountTest extends InteropTestBase {
 
     @org.junit.Test
     public void testReferenceCount() throws Exception {
-        boolean success =
-            readAndVerifyManifest("src/test/resources/interop/c14n/Y4", "signature-manifest.xml", false);
-
-        assertTrue(success);
+        Document doc = getOriginalDocument();
+        signDocument(doc, 31);
+        assertTrue(verifySignature(doc, false));
 
         try {
-            readAndVerifyManifest("src/test/resources/interop/c14n/Y4", "signature-manifest.xml", true);
+            verifySignature(doc, true);
             fail("Failure expected when secure validation is enabled");
         } catch (XMLSecurityException ex) {
             assertTrue(ex.getMessage().contains("references are contained in the Manifest"));
         }
     }
 
-    private boolean readAndVerifyManifest(
-        String directory, String file, boolean secValidation
-    ) throws Exception {
-        String basedir = System.getProperty("basedir");
-        if (basedir != null && !"".equals(basedir)) {
-            directory = basedir + "/" + directory;
+    private Document getOriginalDocument() throws ParserConfigurationException {
+        Document doc = XMLUtils.newDocument();
+
+        Element rootElement = doc.createElementNS("http://ns.example.org/", "root");
+        rootElement.appendChild(doc.createTextNode("Hello World!"));
+        doc.appendChild(rootElement);
+
+        return doc;
+    }
+
+    private void signDocument(Document doc, int refCount) throws Exception {
+        XMLSignature sig = new XMLSignature(doc, "", XMLSignature.ALGO_ID_SIGNATURE_DSA);
+        Element root = doc.getDocumentElement();
+        root.appendChild(sig.getElement());
+
+        for (int i = 0; i < refCount; i++) {
+            Transforms transforms = new Transforms(doc);
+            transforms.addTransform(Transforms.TRANSFORM_ENVELOPED_SIGNATURE);
+            transforms.addTransform(Transforms.TRANSFORM_C14N_WITH_COMMENTS);
+            sig.addDocument("", transforms, Constants.ALGO_ID_DIGEST_SHA1);
         }
 
-        File f = new File(directory + "/" + file);
+        sig.addKeyInfo(getPublicKey());
+        sig.sign(getPrivateKey());
+    }
 
-        org.w3c.dom.Document doc = XMLUtils.read(new FileInputStream(f), false);
-
-        Element manifestElement =
+    private boolean verifySignature(Document doc, boolean secValidation) throws XMLSignatureException, XMLSecurityException {
+        Element sigElement =
             (Element) doc.getElementsByTagNameNS(Constants.SignatureSpecNS,
-                                                 Constants._TAG_SIGNEDINFO).item(0);
-        Manifest manifest = new Manifest(manifestElement, f.toURI().toURL().toString(), secValidation);
-        return manifest.verifyReferences();
+                                                 Constants._TAG_SIGNATURE).item(0);
+        XMLSignature signature = new XMLSignature(sigElement, null, secValidation);
+        return signature.checkSignatureValue(signature.getKeyInfo().getPublicKey());
+    }
+
+    private KeyStore getKeyStore() throws Exception {
+        KeyStore ks = KeyStore.getInstance("JKS");
+        InputStream ksis = new FileInputStream(KEYSTORE_DIRECTORY + "test.jks");
+        ks.load(ksis, KEYSTORE_PASSWORD);
+        ksis.close();
+        return ks;
+    }
+
+    private PublicKey getPublicKey() throws Exception {
+        KeyStore keyStore = getKeyStore();
+        Enumeration<String> aliases = keyStore.aliases();
+        while (aliases.hasMoreElements()) {
+            String alias = aliases.nextElement();
+            if (keyStore.isKeyEntry(alias)) {
+                return keyStore.getCertificate(alias).getPublicKey();
+            }
+        }
+        return null;
+    }
+
+    private PrivateKey getPrivateKey() throws Exception {
+        KeyStore keyStore = getKeyStore();
+        Enumeration<String> aliases = keyStore.aliases();
+        while (aliases.hasMoreElements()) {
+            String alias = aliases.nextElement();
+            if (keyStore.isKeyEntry(alias)) {
+                return (PrivateKey) keyStore.getKey(alias, KEYSTORE_PASSWORD);
+            }
+        }
+        return null;
     }
 
 }
