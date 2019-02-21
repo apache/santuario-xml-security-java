4d6ce8b85ddd968e7bd486b8479334f9041bf26c
Colm O hEigeartaigh <coheigea@apache.org>
2019-02-21 09:36:58 +0000

    SANTUARIO-501 - Enabling EnvelopingSignatureTest

--- src/test/java/javax/xml/crypto/test/dsig/SecureXSLTTest.java
+++ src/test/java/javax/xml/crypto/test/dsig/SecureXSLTTest.java
@@ -76,7 +76,7 @@ public class SecureXSLTTest {
 
     private void testSignature(File signatureFile) throws Exception {
 
-        XMLSignatureFactory fac = XMLSignatureFactory.getInstance("DOM");
+        XMLSignatureFactory fac = XMLSignatureFactory.getInstance("DOM", new org.apache.jcp.xml.dsig.internal.dom.XMLDSigRI());
         File f = new File("doc.xml");
 
         Document doc =
