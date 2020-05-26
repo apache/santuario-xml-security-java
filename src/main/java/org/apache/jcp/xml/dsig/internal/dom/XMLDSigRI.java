f1fc48ac1aff49ec43cedf1e4a6ebdd525c41994
Colm O hEigeartaigh <coheigea@apache.org>
2020-05-26 07:39:27 +0100

    Bumping version for 2.2.1

--- src/main/java/org/apache/jcp/xml/dsig/internal/dom/XMLDSigRI.java
+++ src/main/java/org/apache/jcp/xml/dsig/internal/dom/XMLDSigRI.java
@@ -121,7 +121,7 @@ public final class XMLDSigRI extends Provider {
 
     public XMLDSigRI() {
         /* We are the ApacheXMLDSig provider */
-        super("ApacheXMLDSig", 2.20, INFO);
+        super("ApacheXMLDSig", 2.21, INFO);
 
         final Provider p = this;
         AccessController.doPrivileged(new PrivilegedAction<Void>() {
