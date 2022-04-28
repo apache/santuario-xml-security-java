b4dff4b94a4f73d17aa57d4cbdd8ae50ad09bd95
Colm O hEigeartaigh <coheigea@apache.org>
2022-04-28 09:17:30 +0100

    Bumping provider to 2.32 after release

--- src/main/java/org/apache/jcp/xml/dsig/internal/dom/XMLDSigRI.java
+++ src/main/java/org/apache/jcp/xml/dsig/internal/dom/XMLDSigRI.java
@@ -121,7 +121,7 @@ public final class XMLDSigRI extends Provider {
 
     public XMLDSigRI() {
         /* We are the ApacheXMLDSig provider */
-        super("ApacheXMLDSig", 2.31, INFO);
+        super("ApacheXMLDSig", 2.32, INFO);
 
         final Provider p = this;
         AccessController.doPrivileged(new PrivilegedAction<Void>() {
