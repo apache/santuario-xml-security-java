452556979bfaec9d798917c599fb8e405768314c
Isaac Jensen <isaac.jensen@outlook.com>
2022-07-06 16:01:00 -0700

    addressing comments

--- src/main/java/org/apache/xml/security/stax/ext/XMLSecurityConstants.java
+++ src/main/java/org/apache/xml/security/stax/ext/XMLSecurityConstants.java
@@ -43,13 +43,14 @@ public class XMLSecurityConstants {
     public static final XMLOutputFactory xmlOutputFactoryNonRepairingNs;
 
     private static final SecureRandom SECURE_RANDOM;
-	private static final String RANDOM_ALGORITHM_KEY = "xmlsec.securerandom.algorithm";
+	private static final String RANDOM_ALGORITHM_KEY = "org.apache.xml.security.securerandom.algorithm";
     private static JAXBContext jaxbContext;
     private static Schema schema;
 
     static {
         try {
-			String PrngAlgorithm = System.getProperty(RANDOM_ALGORITHM_KEY,"SHA1PRNG");
+			String PrngAlgorithm = System.getProperty(RANDOM_ALGORITHM_KEY, SecureRandom.getInstanceStrong().getAlgorithm());
+
             SECURE_RANDOM = SecureRandom.getInstance(PrngAlgorithm);
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
