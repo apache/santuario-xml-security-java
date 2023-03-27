6cbc4872e24ca2d49f33a231e1bdefd57d6477e0
Colm O hEigeartaigh <coheigea@apache.org>
2023-03-27 09:57:29 +0100

    Updating version after release

--- src/main/java/org/apache/jcp/xml/dsig/internal/dom/XMLDSigRI.java
+++ src/main/java/org/apache/jcp/xml/dsig/internal/dom/XMLDSigRI.java
@@ -121,7 +121,7 @@ public final class XMLDSigRI extends Provider {
 
     public XMLDSigRI() {
         /* We are the ApacheXMLDSig provider */
-        super("ApacheXMLDSig", 3.02, INFO);
+        super("ApacheXMLDSig", 3.03, INFO);
 
         final Provider p = this;
         AccessController.doPrivileged(new PrivilegedAction<Void>() {
