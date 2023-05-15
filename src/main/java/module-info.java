c61127c84674323a9a6c3d4cd80a85d9c122f000
David Matějček <david.matejcek@omnifish.ee>
2023-05-15 22:28:14 +0200

    Fixed dependencies based on tests with GlassFish and Metro Webservices
    Signed-off-by: David Matějček <david.matejcek@omnifish.ee>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -22,17 +22,40 @@
  */
 module org.apache.santuario.xmlsec {
 
-    requires jakarta.activation;
-    requires jakarta.xml.bind;
+    requires transitive jakarta.activation;
+    requires transitive jakarta.xml.bind;
+    requires java.base;
     requires java.management;
+    requires java.xml;
     requires java.xml.crypto;
     requires org.apache.commons.codec;
 
+    exports org.apache.jcp.xml.dsig.internal.dom;
     exports org.apache.xml.security;
+    exports org.apache.xml.security.algorithms;
+    exports org.apache.xml.security.algorithms.implementations;
+    exports org.apache.xml.security.c14n;
+    exports org.apache.xml.security.c14n.helper;
+    exports org.apache.xml.security.c14n.implementations;
     exports org.apache.xml.security.configuration;
+    exports org.apache.xml.security.encryption;
+    exports org.apache.xml.security.exceptions;
+    exports org.apache.xml.security.keys;
+    exports org.apache.xml.security.keys.content;
+    exports org.apache.xml.security.keys.content.keyvalues;
+    exports org.apache.xml.security.keys.content.x509;
+    exports org.apache.xml.security.keys.keyresolver.implementations;
+    exports org.apache.xml.security.keys.storage.implementations;
     exports org.apache.xml.security.signature;
     exports org.apache.xml.security.stax.ext;
+    exports org.apache.xml.security.transforms;
+    exports org.apache.xml.security.transforms.implementations;
+    exports org.apache.xml.security.transforms.params;
+    exports org.apache.xml.security.utils;
+    exports org.apache.xml.security.utils.resolver;
+    exports org.apache.xml.security.utils.resolver.implementations;
 
+    opens org.apache.jcp.xml.dsig.internal.dom;
     opens org.apache.xml.security;
     opens org.apache.xml.security.binding.excc14n;
     opens org.apache.xml.security.binding.xmldsig;
@@ -41,4 +64,10 @@ module org.apache.santuario.xmlsec {
     opens org.apache.xml.security.binding.xmlenc11;
     opens org.apache.xml.security.binding.xop;
     opens org.apache.xml.security.configuration;
+    opens org.apache.xml.security.algorithms.implementations;
+    opens org.apache.xml.security.c14n.implementations;
+    opens org.apache.xml.security.keys.keyresolver.implementations;
+    opens org.apache.xml.security.keys.storage.implementations;
+    opens org.apache.xml.security.transforms.implementations;
+    opens org.apache.xml.security.utils.resolver.implementations;
 }
