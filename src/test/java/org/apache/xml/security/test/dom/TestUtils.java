7c90e2245eac33e6d283f18f5cce1b76fed96029
jrihtarsic <jrihtarsic1@gmail.com>
2024-10-24 08:22:16 +0200

    [SANTUARIO-623] fix the transformation XPATH2 filtering. (#389)
    * [SANTUARIO-623] fix the transformation XPATH2 filtering.
    
    * [SANTUARIO-623] update the xml example and add missing license notice
    
    ---------
    
    Co-authored-by: RIHTARSIC Joze <joze.rihtarsic@ext.ec.europa.eu>

--- src/test/java/org/apache/xml/security/test/dom/TestUtils.java
+++ src/test/java/org/apache/xml/security/test/dom/TestUtils.java
@@ -21,14 +21,22 @@ package org.apache.xml.security.test.dom;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.xml.security.parser.XMLParserException;
 import org.apache.xml.security.utils.Constants;
+import org.apache.xml.security.utils.XMLUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class TestUtils {
 
     private static final DocumentBuilderFactory DBF = DocumentBuilderFactory.newInstance();
     private static final boolean isJava11Compatible;
+    // The class-path to test resources
+    private static final String RESOURCE_PATH = "/org/apache/xml/security/test/javax/xml/crypto/dsig/";
 
     static {
         DBF.setNamespaceAware(true);
@@ -70,6 +78,39 @@ public class TestUtils {
 
     public static Document newDocument() throws ParserConfigurationException {
         return DBF.newDocumentBuilder().newDocument();
+    }
+
+    /**
+     * Create a test method to read input XML Fragment bytes and return a Document with root element "ROOT".
+     * The XML fragment are wrapped in a "ROOT" element to make it a valid XML document.
+     *
+     * @param xmlFragment the XML fragment bytes
+     *                    (e.g. "<a>text 2</a><a><b>text 2</b></a>".getBytes())
+     * @return the Document with root element "ROOT"
+     */
+    public static Document xmlFragmentToDocument(byte[] xmlFragment) throws XMLParserException, IOException {
+        if (xmlFragment == null || xmlFragment.length == 0) {
+            throw new IllegalArgumentException("XML fragment cannot be null or empty");
+        }
+
+        String xml = new String(xmlFragment, StandardCharsets.UTF_8);
+        String xmlDocument = "<ROOT>" + xml + "</ROOT>";
+
+        try (ByteArrayInputStream is = new ByteArrayInputStream(xmlDocument.getBytes(StandardCharsets.UTF_8))) {
+            return XMLUtils.read(is, true);
+        }
+    }
+
+    /**
+     * Get a test document from a classpath resource.
+     * @param fileName the file name of the resource
+     * @return the Document
+     * @throws XMLParserException if an error occurs while parsing the XML document
+     */
+    public static Document getTestDocumentFromResource(String fileName) throws XMLParserException {
+        // read document from classpath resource
+        return XMLUtils.read(TestUtils.class
+                .getResourceAsStream(RESOURCE_PATH + fileName), true);
     }
 
     public static boolean isJava11Compatible() {
