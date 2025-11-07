92df1bcd70a96e4497f185cad9bb2f1d2abc3923
Ihor Kuzmanenko <i.kuzmanenko@samsung.com>
2025-11-07 21:13:21 +0200

    review: fixed access modifiers, added license and more comments in the test

--- src/test/java/org/apache/xml/security/utils/XMLUtilsTest.java
+++ src/test/java/org/apache/xml/security/utils/XMLUtilsTest.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.xml.security.utils;
 
 import org.junit.jupiter.api.AfterEach;
@@ -16,6 +34,22 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * This test checks {@link XMLUtils} class methods, responsible for Base64 values formatting in XML documents.
+ * Since it is a utility class with static methods, and it is configured with system properties,
+ * we need to reload the class after system properties are set in each test case.
+ * This test uses a special implementation of {@link ClassLoader} to achieve this and calls {@code XMLUtils} methods
+ * using reflection.
+ *
+ * There are three methods producing Base64-encoded data in {@code XMLUtils}:
+ * <ul>
+ *     <li>{@link XMLUtils#encodeToString(byte[])}</li>
+ *     <li>{@link XMLUtils#encodeElementValue(byte[])}</li>
+ *     <li>{@link XMLUtils#encodeStream(OutputStream)}</li> (creates a wrapper stream, which applies the same encoding
+ *         as {@code encodeToString(byte[])})
+ * </ul>
+ * In the tests, formatting of the outputs of these methods is checked.
+ */
 public class XMLUtilsTest {
     private static final byte[] data = new byte[60]; // long enough for a line break in MIME encoding
 
@@ -24,7 +58,7 @@ public class XMLUtilsTest {
 
     @BeforeEach
     public void createClassLoader() {
-        /* create custom classloader to reload class in each test */
+        /* create custom classloader to reload XMLUtils class and its nested classes in each test */
         ClassLoader parent = getClass().getClassLoader();
         Collection<Class<?>> classesToReload = List.of(
                 XMLUtils.class,
@@ -32,6 +66,11 @@ public class XMLUtilsTest {
                 XMLUtils.Base64LineSeparator.class
         );
         classLoader = new ReloadingClassLoader(parent, classesToReload);
+
+        /*
+         * XMLUtils instantiates XMLParserImpl, but its package is not exported,
+         * thus unavailable for the new classloader.
+         */
         ModuleLayer.boot().findModule("org.apache.santuario.xmlsec").orElseThrow()
                 .addOpens("org.apache.xml.security.parser", classLoader.getUnnamedModule());
     }
@@ -221,10 +260,19 @@ public class XMLUtilsTest {
         }
     }
 
+    /**
+     * This implementation of {@code ClassLoader} reloads given classes from bytecode,
+     * even if they are already loaded by the parent class loader.
+     */
     private static class ReloadingClassLoader extends ClassLoader {
         private Collection<String> classNames;
 
-        public ReloadingClassLoader(ClassLoader parent, Collection<Class<?>> classes) {
+        /**
+         * Creates new class loader.
+         * @param parent    Parent class loader.
+         * @param classes   Set of classes to be forcefully reloaded
+         */
+        private ReloadingClassLoader(ClassLoader parent, Collection<Class<?>> classes) {
             super("TestClassLoader", parent);
             this.classNames = classes.stream().map(Class::getName).collect(Collectors.toSet());
         }
