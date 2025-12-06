9e3fdac5f6930ebb721928072c63ce2cb98ad145
Ihor Kuzmanenko <real.kuzjka@gmail.com>
2025-12-06 16:30:26 +0200

    added high-level formatting tests for signature and encryption

---
+++ src/test/java/org/apache/xml/security/test/dom/signature/SignatureFormattingTest.java
@@ -0,0 +1,178 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.xml.security.test.dom.signature;
+
+import org.apache.xml.security.Init;
+import org.apache.xml.security.formatting.FormattingChecker;
+import org.apache.xml.security.formatting.FormattingCheckerFactory;
+import org.apache.xml.security.formatting.FormattingTest;
+import org.apache.xml.security.signature.XMLSignature;
+import org.apache.xml.security.signature.XMLSignatureByteInput;
+import org.apache.xml.security.signature.XMLSignatureInput;
+import org.apache.xml.security.test.dom.DSNamespaceContext;
+import org.apache.xml.security.test.dom.TestUtils;
+import org.apache.xml.security.utils.Constants;
+import org.apache.xml.security.utils.ElementProxy;
+import org.apache.xml.security.utils.XMLUtils;
+import org.apache.xml.security.utils.resolver.ResourceResolverContext;
+import org.apache.xml.security.utils.resolver.ResourceResolverException;
+import org.apache.xml.security.utils.resolver.ResourceResolverSpi;
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import javax.xml.crypto.dsig.DigestMethod;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.security.cert.X509Certificate;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+/**
+ * This is a {@link FormattingTest}, it is expected to be run with different system properties
+ * to check various formatting configurations.
+ *
+ * The test creates a detached signature with a single reference and uses mock resource resolver.
+ * RSA-2048 and SHA-512 are used to create longer binary values.
+ */
+@FormattingTest
+public class SignatureFormattingTest {
+    private final static byte[] MOCK_DATA = new byte[]{ 0x0a, 0x0b, 0x0c, 0x0d };
+
+    private final FormattingChecker formattingChecker;
+    private KeyStore keyStore;
+    private XPath xpath;
+    private ResourceResolverSpi resolver;
+
+    public SignatureFormattingTest() throws Exception {
+        Init.init();
+        ElementProxy.setDefaultPrefix(Constants.SignatureSpecNS, "ds");
+        formattingChecker = FormattingCheckerFactory.getFormattingChecker();
+        keyStore = KeyStore.getInstance("PKCS12");
+        try (InputStream in = getClass()
+                .getResourceAsStream("/org/apache/xml/security/samples/input/rsa.p12")) {
+            keyStore.load(in, "xmlsecurity".toCharArray());
+        } catch (IOException | GeneralSecurityException e) {
+            fail("Cannot load test keystore", e);
+        }
+
+        resolver = new TestResourceResolver(MOCK_DATA);
+
+        XPathFactory xPathFactory = XPathFactory.newInstance();
+        xpath = xPathFactory.newXPath();
+        xpath.setNamespaceContext(new DSNamespaceContext());
+    }
+
+    @Test
+    public void testSignatureFormatting() throws Exception {
+        /* this test checks formatting of base64Binary values */
+        Document doc = createDocument();
+
+        String docStr;
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            XMLUtils.outputDOM(doc, out);
+            out.flush();
+            docStr = out.toString(StandardCharsets.UTF_8);
+        }
+
+        formattingChecker.checkDocument(docStr);
+
+        XPathFactory xPathFactory = XPathFactory.newInstance();
+        XPath xpath = xPathFactory.newXPath();
+        xpath.setNamespaceContext(new DSNamespaceContext());
+
+        Element digest = findElementByXpath("//ds:DigestValue[1]", doc);
+        formattingChecker.checkBase64Value(digest.getTextContent());
+
+        Element signatureValue = findElementByXpath("//ds:SignatureValue[1]", doc);
+        formattingChecker.checkBase64ValueWithSpacing(signatureValue.getTextContent());
+
+        Element x509certValue = findElementByXpath("//ds:X509Certificate[1]", doc);
+        formattingChecker.checkBase64ValueWithSpacing(x509certValue.getTextContent());
+    }
+
+    @Test
+    public void testSignVerify() throws Exception {
+        /* this test checks the signature can be verified with given formatting settings */
+        Document doc = createDocument();
+        Element signatureElement = findElementByXpath("//ds:Signature[1]", doc);
+        XMLSignature signature = new XMLSignature(signatureElement, null);
+        signature.addResourceResolver(resolver);
+
+        PublicKey publicKey = keyStore.getCertificate("test").getPublicKey();
+        assertTrue(signature.checkSignatureValue(publicKey));
+    }
+
+    private Document createDocument() throws Exception {
+        Document doc = TestUtils.newDocument();
+
+        XMLSignature signature = new XMLSignature(doc, null, XMLSignature.ALGO_ID_SIGNATURE_RSA_SHA512);
+        signature.addResourceResolver(resolver);
+
+        signature.addDocument("some.resource", null, DigestMethod.SHA512);
+
+        PrivateKey privateKey = (PrivateKey) keyStore.getKey("test", "xmlsecurity".toCharArray());
+        X509Certificate certificate = (X509Certificate) keyStore.getCertificate("test");
+
+        signature.addKeyInfo(certificate);
+        signature.sign(privateKey);
+
+        doc.appendChild(signature.getElement());
+
+        return doc;
+    }
+
+    private Element findElementByXpath(String expression, Node node) throws XPathExpressionException {
+        return (Element) xpath.evaluate(expression, node, XPathConstants.NODE);
+    }
+
+    /**
+     * Resolver implementation which resolves every URI to the same given mock data.
+     */
+    private static class TestResourceResolver extends ResourceResolverSpi {
+        private byte[] mockData;
+
+        /**
+         * Creates new resolver.
+         * @param mockData  Mock data bytes
+         */
+        public TestResourceResolver(byte[] mockData) {
+            this.mockData = mockData;
+        }
+
+        @Override
+        public XMLSignatureInput engineResolveURI(ResourceResolverContext context) throws ResourceResolverException {
+            return new XMLSignatureByteInput(mockData);
+        }
+
+        @Override
+        public boolean engineCanResolveURI(ResourceResolverContext context) {
+            return true;
+        }
+    }
+}
---
+++ src/test/java/org/apache/xml/security/test/stax/signature/SignatureFormattingTest.java
@@ -0,0 +1,181 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.xml.security.test.stax.signature;
+
+import org.apache.xml.security.Init;
+import org.apache.xml.security.exceptions.XMLSecurityException;
+import org.apache.xml.security.formatting.FormattingChecker;
+import org.apache.xml.security.formatting.FormattingCheckerFactory;
+import org.apache.xml.security.formatting.FormattingTest;
+import org.apache.xml.security.stax.ext.*;
+import org.apache.xml.security.stax.securityEvent.*;
+import org.apache.xml.security.stax.securityToken.SecurityTokenConstants;
+import org.apache.xml.security.test.dom.DSNamespaceContext;
+import org.apache.xml.security.test.stax.utils.XmlReaderToWriter;
+import org.apache.xml.security.utils.Constants;
+import org.apache.xml.security.utils.ElementProxy;
+import org.apache.xml.security.utils.XMLUtils;
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This is a {@link FormattingTest}, it is expected to be run with different system properties
+ * to check various formatting configurations.
+ *
+ * The test adds an XML signature to a sample document using StAX API.
+ * Formatting of base64binary values is then checked.
+ * Also, signature verification with StAX API is performed to ensure different formatting can be consumed.
+ */
+@FormattingTest
+public class SignatureFormattingTest {
+    private final FormattingChecker formattingChecker;
+    private KeyStore keyStore;
+    private XPath xpath;
+    private XMLInputFactory xmlInputFactory;
+
+    public SignatureFormattingTest() throws Exception {
+        Init.init();
+        ElementProxy.setDefaultPrefix(Constants.SignatureSpecNS, "ds");
+        formattingChecker = FormattingCheckerFactory.getFormattingChecker();
+        keyStore = KeyStore.getInstance("PKCS12");
+        try (InputStream in = getClass()
+                .getResourceAsStream("/org/apache/xml/security/samples/input/rsa.p12")) {
+            keyStore.load(in, "xmlsecurity".toCharArray());
+        } catch (IOException | GeneralSecurityException e) {
+            fail("Cannot load test keystore", e);
+        }
+
+        XPathFactory xPathFactory = XPathFactory.newInstance();
+        xpath = xPathFactory.newXPath();
+        xpath.setNamespaceContext(new DSNamespaceContext());
+
+        xmlInputFactory = XMLInputFactory.newInstance();
+    }
+
+    @Test
+    public void testSignatureFormatting() throws Exception {
+        /* this test checks formatting of base64Binary values */
+        byte[] documentBytes = createDocument();
+
+        /*
+         * The document retains a part of the original document, so we can't check the whole file linebreaks,
+         * i.e. formattingChecker.checkDocument(docStr);
+         */
+
+        /* parse as DOM to check base64 values */
+        Document document;
+        try (InputStream in = new ByteArrayInputStream(documentBytes)) {
+            document = XMLUtils.read(in, false);
+        }
+
+        /*
+         * In StAX implementation long element values are not surrounded by linebreaks,
+         * i.e. checkBase64ValueWithSpacing is not applicable.
+         */
+        Element signatureValue =
+                (Element) xpath.evaluate("//ds:SignatureValue", document, XPathConstants.NODE);
+        formattingChecker.checkBase64Value(signatureValue.getTextContent());
+
+        Element x509certificate =
+                (Element) xpath.evaluate("//ds:X509Certificate", document, XPathConstants.NODE);
+        formattingChecker.checkBase64Value(x509certificate.getTextContent());
+    }
+    
+    @Test
+    public void testSignVerify() throws Exception {
+        /* this test checks the signature can be verified with given formatting settings */
+        byte[] documentBytes = createDocument();
+        
+        XMLSecurityProperties properties = new XMLSecurityProperties();
+        InboundXMLSec inboundXMLSec = XMLSec.getInboundWSSec(properties);
+        
+        try (InputStream in = new ByteArrayInputStream(documentBytes)) {
+            XMLStreamReader reader = xmlInputFactory.createXMLStreamReader(in, StandardCharsets.UTF_8.name());
+            VerificationSecurityEventListener listener = new VerificationSecurityEventListener();
+            XMLStreamReader xmlSecReader = inboundXMLSec.processInMessage(reader, null, listener);
+            // read the document
+            while (xmlSecReader.hasNext()) xmlSecReader.next();
+            xmlSecReader.close();
+            assertTrue(listener.isSignatureVerified());
+        }
+    }
+
+    private byte[] createDocument() throws Exception {
+        PrivateKey privateKey = (PrivateKey) keyStore.getKey("test", "xmlsecurity".toCharArray());
+        X509Certificate certificate = (X509Certificate) keyStore.getCertificate("test");
+
+        XMLSecurityProperties properties = new XMLSecurityProperties();
+        properties.setActions(List.of(XMLSecurityConstants.SIGNATURE));
+        properties.setSignatureKey(privateKey);
+        properties.setSignatureCerts(new X509Certificate[]{ certificate });
+        SecurePart securePart =
+                new SecurePart(new QName("urn:example:po", "PaymentInfo"), SecurePart.Modifier.Content);
+        properties.addSignaturePart(securePart);
+        properties.setSignatureAlgorithm("http://www.w3.org/2001/04/xmldsig-more#rsa-sha512");
+        properties.setSignatureKeyIdentifier(SecurityTokenConstants.KeyIdentifier_X509KeyIdentifier);
+
+        String plaintextResource = "/ie/baltimore/merlin-examples/merlin-xmlenc-five/plaintext.xml";
+        try (InputStream in = getClass().getResourceAsStream(plaintextResource);
+             ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            XMLStreamReader reader = xmlInputFactory.createXMLStreamReader(in);
+            OutboundXMLSec outboundXMLSec = XMLSec.getOutboundXMLSec(properties);
+            XMLStreamWriter writer = outboundXMLSec.processOutMessage(out, StandardCharsets.UTF_8.name(), null);
+            XmlReaderToWriter.writeAllAndClose(reader, writer);
+            return out.toByteArray();
+        }
+    }
+    
+    private static class VerificationSecurityEventListener implements SecurityEventListener {
+        boolean signatureVerified = false;
+
+        public boolean isSignatureVerified() {
+            return signatureVerified;
+        }
+
+        @Override
+        public void registerSecurityEvent(SecurityEvent securityEvent) throws XMLSecurityException {
+            if (SecurityEventConstants.SignatureValue.equals(securityEvent.getSecurityEventType())) {
+                SignatureValueSecurityEvent event = (SignatureValueSecurityEvent) securityEvent;
+                assertNotNull(event.getSignatureValue());
+                signatureVerified = true;
+            } else if (SecurityEventConstants.SignedElement.equals(securityEvent.getSecurityEventType())) {
+                SignedElementSecurityEvent event = (SignedElementSecurityEvent) securityEvent;
+                assertTrue(event.isSigned());
+            }
+        }
+    }
+}
